0d15cb40b926c9d0ac032bf12eeadf1661bf9e57
Tomoya ONDA <onda.tomoya@gmail.com>
2023-01-10 00:25:24 +0900

    font rip tool

---
+++ Tools/Tiny2Tools/Tiny2Tools/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Tiny2Tools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save As";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "bin";
+            this.openFileDialog1.Filter = "*.bin|*.*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(409, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 282);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tiny2Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Tools/Tiny2Tools/Tiny2Tools/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Tiny2Tools
+{
+    public partial class Form1 : Form
+    {
+        const byte RedVal = 0x00;
+        const byte GreenVal = 0x01;
+        const byte BlueVal = 0x02;
+        const byte YellowVal = 0x03;
+
+        Byte[] buf;
+
+        Bitmap bitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            Color[] colorList = new Color[4];
+
+            colorList[(int)RedVal] = Color.FromArgb(255, 255, 0, 0);
+            colorList[(int)GreenVal] = Color.FromArgb(255, 0, 255, 0);
+            colorList[(int)BlueVal] = Color.FromArgb(255, 0, 0, 255);
+            colorList[(int)YellowVal] = Color.FromArgb(255, 255, 255, 0);
+
+            /*
+            // stream load
+            FileStream file = new FileStream("test.bin", FileMode.Open, FileAccess.Read);
+            Byte[] data = new Byte[file.Length];
+            */
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = saveFileDialog1.ShowDialog();
+
+            bitmap.Save(saveFileDialog1.FileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                FileStream file = File.OpenRead(openFileDialog1.FileName);
+                int count = (int)file.Length;
+                buf = new Byte[count];
+                file.Read(buf, 0, count);
+                file.Dispose();
+            }
+
+            CreateBitmap();
+
+            pictureBox1.Image = bitmap;
+            this.Invalidate();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void CreateBitmap()
+        {
+            bitmap = new Bitmap(256, 256);
+
+            /*
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
+                    Color color = Color.FromArgb(255, i, j, 0);
+
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+             * */
+
+            int x = 0, y = 0;
+
+            for (int code = 0; code < 64; code++ )
+            {
+                y = 0;
+                int offset = 0x2e00;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    int val = (int)buf[code * 8 + i + offset];
+                    Debug.WriteLine(val.ToString("x"));
+                    x = 0;
+
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if ((val & 0x80) != 0)
+                        {
+                            if (y < 4)
+                            {
+                                bitmap.SetPixel((code % 32) * 8 + x, (code / 32) * 8 + y, Color.White);
+                            }
+                            else
+                            {
+                                bitmap.SetPixel((code % 32) * 8 + x, (code / 32) * 8 + y, Color.LightGreen);
+                            }
+                        }
+                        else
+                        {
+                            bitmap.SetPixel((code % 32) * 8 + x, (code / 32) * 8 + y, Color.Black);
+                        }
+                        val = val << 1;
+                        x++;
+                    }
+                    y++;
+                }
+            }
+
+        }
+    }
+
+}
